90c263c06808c3106e16130c5e5d3632e0c5b267
AliMahmoud26 <AliMahmoudHelal26@gmail.com>
2024-06-07 19:44:29 +0300

    commiting the initalized files of the react project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Skills from "./components/Skills"
+import Timeline from "./components/Timeline"
+import Projects from "./components/Projects"
+import Games from "./subComponents/Games"
+import Footer from "./components/Footer"
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <Timeline />
+      <Projects />
+      <Games />
+      <Footer />
+    </>
+  )
+}
+
+export default App
