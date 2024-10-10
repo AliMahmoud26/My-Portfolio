ca170ef041169b305d8d2c03759d948a0cc97d51
AliMahmoud26 <AliMahmoudHelal26@gmail.com>
2024-10-10 17:41:40 +0300

    updaing my footer reach-me section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,8 @@ const Footer = () => {
         <div className="other-links">
           <div className="email">
             <h4>REACH ME AT</h4>
-            <a href="mailto:alimahmoud.cs@outlook.com">Email Me</a>
+            <a href="mailto:alimahmoud.cs@outlook.com" style={{marginRight: "1.5rem"}}>Outlook</a>
+            <a href="mailto:alimah.engineer@gmail.com">Gmail</a>
           </div>
           <div className="page-links">
             <h4>Page Links</h4>
@@ -31,7 +32,7 @@ const Footer = () => {
             </div>
           </div>
           <div className="email-box">
-            <a href="mailto:alimahmoud.cs@outlook.com" title='My Email'>SAY HELLO.</a>
+            <a href="mailto:alimahmoud.cs@outlook.com" title='Outlook'>SAY HELLO.</a>
           </div>
         </div>
       </div>
